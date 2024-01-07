e5601e3de3e6d6416c916915f0285ec4e4dc8d9a
NaokiHori <36466440+NaokiHori@users.noreply.github.com>
2024-01-07 13:40:49 +0900

    [add] topics in card

--- scripts/card/main.py
+++ scripts/card/main.py
@@ -1,29 +1,28 @@
-# This source aims to create a SVG file
-#   containing information about a repository
-#   from several json files fetched by GitHub Actions
-# The format of is motivated by the one
-#   proposed in https://github.com/anuraghazra/github-readme-stats
+class Font:
+    def __init__(self, h: float):
+        # h/w rate for "Courier New"
+        rate = 0.607
+        self.h = h
+        self.w = h * rate
 
-import os
-import sys
-import json
-import math
-import requests
-from datetime import date
+font_title = Font(20)
+font_descr = Font(16)
 
-
-def get_info(name):
-    def get_lastcommit(data):
+def get_info(name: str):
+    def get_lastcommit(data: str):
         # do what
         #   https://docs.python.org/3/library/datetime.html#datetime.datetime.fromisoformat
         # does
         # dirty hack before 3.11
+        from datetime import date
         data = data.split("T")[0]
         data = date.fromisoformat(data)
         data = data.strftime("%b %d %Y")
         return data
     # extract repository name and its description
     # ref: https://docs.github.com/en/rest/repos/repos?apiVersion=2022-11-28#get-a-repository
+    import os
+    import requests
     token = os.environ.get(key="GITHUB_TOKEN")
     r = requests.get(
             url=f"https://api.github.com/repos/NaokiHori/{name}",
@@ -34,299 +33,224 @@ def get_info(name):
             },
     )
     data = r.json()
-    print(data)
-    result = {
+    info = {
             "name": name,
             "descr": data["description"],
+            "topics": data["topics"],
             "lang": data["language"],
             "nstars": str(data["stargazers_count"]),
             "lastcommit": get_lastcommit(data["updated_at"]),
     }
+    return info
+
+def set_margin(domain: dict) -> float:
+    # left-right margin
+    result = 1.5 * font_title.w
     return result
 
-def get_header(width, height):
-    # header of svg
-    string = (
-        f"<svg "
-        f"width=\"{width}\" "
-        f"height=\"{height}\" "
-        f"viewBox=\"0 0 {width} {height}\" "
-        f"fill=\"none\" "
-        f"xmlns=\"http://www.w3.org/2000/svg\" "
-        f"role=\"img\">\n"
-    )
-    return string
+def get_title(info: dict, domain: dict, margin: float) -> dict:
+    domain["height"] += 1.5 * font_title.h
+    result = {
+        "text": info["name"],
+        "x": margin,
+        "y": domain["height"],
+    }
+    domain["height"] += 1.5 * font_title.h
+    return result
 
-def get_footer():
-    # footer of svg
-    string = f"</svg>\n"
-    return string
-
-def get_styles(font, h_title, h_descr):
-    # CSS styles, including font specifier and icon colour
-    string = (
-        f"  <style>\n"
-        f"    .title-text {{\n"
-        f"      font: 600 {h_title}px {font};\n"
-        f"      fill: #2f80ed;\n"
-        f"    }}\n"
-        f"    .descr-text {{\n"
-        f"      font: 400 {h_descr}px {font};\n"
-        f"      fill: #434d58;\n"
-        f"    }}\n"
-        f"    .star {{\n"
-        f"      stroke: #586069;\n"
-        f"      stroke-width: 1.5;\n"
-        f"      fill: \"none\";\n"
-        f"    }}\n"
-        f"    .icon {{\n"
-        f"      fill: #586069;\n"
-        f"    }}\n"
-        f"  </style>\n"
-    )
-    return string
-
-def get_edge_r(width_svg, height_svg):
-    # edge rectangle
-    offset = 0.5 # px
-    corner = offset * 9
-    height = height_svg - 2. * offset
-    width  = width_svg  - 2. * offset
-    string = (
-        f"  <rect "
-        f"data-testid=\"card edge\" "
-        f"x=\"{offset}\" "
-        f"y=\"{offset}\" "
-        f"rx=\"{corner}\" "
-        f"ry=\"{corner}\" "
-        f"width=\"{width}\" "
-        f"height=\"{height}\" "
-        f"stroke=\"#E4E2E2\" "
-        f"fill=\"#FFFEFE\" "
-        f"stroke-opacity=\"1\" "
-        f"/>\n"
-    )
-    return string
-
-def get_row1st(xoffset, yoffset, iconsize, name):
-    # first row, repository logo and title
-    text_xoffset = 2 * iconsize
-    string = (
-        f"  <g data-testid=\"title\" transform=\"translate({xoffset}, {yoffset})\">\n"
-        f"    <g data-testid=\"title-icon\" transform=\"translate(0, 0)\">\n"
-        f"      <svg\n"
-        f"        class=\"icon\"\n"
-        f"        x=\"0\"\n"
-        f"        y=\"-13\"\n"
-        f"        viewBox=\"0 0 {iconsize} {iconsize}\"\n"
-        f"        version=\"1.1\"\n"
-        f"        width=\"{iconsize}\"\n"
-        f"        height=\"{iconsize}\"\n"
-        f"      >\n"
-        f"        <path fill-rule=\"evenodd\" d=\"M2 2.5A2.5 2.5 0 014.5 0h8.75a.75.75 0 01.75.75v12.5a.75.75 0 01-.75.75h-2.5a.75.75 0 110-1.5h1.75v-2h-8a1 1 0 00-.714 1.7.75.75 0 01-1.072 1.05A2.495 2.495 0 012 11.5v-9zm10.5-1V9h-8c-.356 0-.694.074-1 .208V2.5a1 1 0 011-1h8zM5 12.25v3.25a.25.25 0 00.4.2l1.45-1.087a.25.25 0 01.3 0L8.6 15.7a.25.25 0 00.4-.2v-3.25a.25.25 0 00-.25-.25h-3.5a.25.25 0 00-.25.25z\"/>\n"
-        f"      </svg>\n"
-        f"    </g>\n"
-        f"    <g data-testid=\"title-text\" transform=\"translate({text_xoffset}, 0)\">\n"
-        f"      <text x=\"0\" y=\"0\" class=\"title-text\">\n"
-        f"        {name}\n"
-        f"      </text>\n"
-        f"    </g>\n"
-        f"  </g>\n"
-    )
-    return string, yoffset + 2 * iconsize
-
-def get_row2nd(xoffset, yoffset, width, h_descr, w_descr, descr):
-    # second row: repository description
-    # maximum number of characters allowed
-    # subtract left / right margins (2 x xoffset)
-    maxchar = (width - 2 * xoffset) // w_descr
-    words = descr.split(" ")
-    rows = list()
-    row  = list()
-    cntr = 0
-    for word in words:
-        cntr += len(word) + 1
-        if cntr > maxchar:
-            rows.append(row)
-            row = list()
-            cntr = len(word) + 1
-        row.append(word)
-    if row:
-        rows.append(row)
-    string = list()
-    string.append((
-        f"  <g data-testid=\"description\" transform=\"translate({xoffset}, {yoffset})\">\n"
-    ))
-    for cntr, row in enumerate(rows):
-        words = " ".join(row)
-        dy = 2. * h_descr * cntr
-        row = f"    <text class=\"descr-text\" x=\"0\" y=\"{dy}\">{words}</text>\n"
-        string.append((row))
-    string.append((
-        f"  </g>\n"
-    ))
-    string = "".join(string)
-    return string, yoffset + 2.5 * h_descr * len(rows)
-
-def draw_star(size):
-    offset = 0.75 * size
-    # star center
-    x0 = 0.5 * size
-    y0 = 0.5 * size
-    # radii for each 5 points
-    ro = 0.425 * size
-    ri = 0.500 * ro
-    # coordinates
-    xs = list()
-    ys = list()
-    for cnt in range(10):
-        # theta
-        t = 2. * math.pi * cnt / 10 - 0.5 * math.pi
-        # radius
-        r = ro if cnt % 2 == 0 else ri
-        xs.append(x0 + r * math.cos(t))
-        ys.append(y0 + r * math.sin(t))
-    # svg path tag
-    path = list()
-    for c, (x, y) in enumerate(zip(xs, ys)):
-        prefix = "M" if c ==           0 else "L"
-        suffix = "Z" if c == len(xs) - 1 else " "
-        path.append(f"{prefix} {x: .1f} {y: .1f} {suffix}")
-    path = "".join(path)
-    string = (
-        f"    <g data-testid=\"star-icon\" transform=\"translate(-{offset}, -{offset})\">\n"
-        f"      <svg\n"
-        f"        class=\"star\"\n"
-        f"        x=\"0\"\n"
-        f"        y=\"0\"\n"
-        f"        viewBox=\"0 0 {size} {size}\"\n"
-        f"        version=\"1.1\"\n"
-        f"        width=\"{size}\"\n"
-        f"        height=\"{size}\"\n"
-        f"      >\n"
-        f"        <path d=\"{path}\"/>\n"
-        f"      </svg>\n"
-        f"    </g>\n"
-    )
-    return string
-
-def get_row3rd(xoffset, yoffset, iconsize, w_descr, h_title, info):
-    def get_lang_color_dict(key):
-        # language vs color table
-        with open("scripts/card/colortable.json", "r") as fp:
-            table = json.load(fp)
-        if key in table.keys():
-            return table[key]
+def get_descrs(info: dict, domain: dict, margin: float) -> list:
+    width = domain["width"] - 2 * margin
+    chars = info["descr"]
+    # split the whole description into multiple lines so that they fit the card horizontally
+    texts = list()
+    text = ""
+    w = 0
+    for char in chars:
+        if w >= width:
+            texts.append(text)
+            w = 0
+            text = char
         else:
-            # return black if not found
-            print("language color is not defined: ", key)
-            return "#000000"
-    lang = info["lang"]
-    nstars = info["nstars"]
-    lastcommit = info["lastcommit"]
-    # third row: primary language, number of stars, last update date
-    string = list()
-    # language, can be None
-    if lang:
-        string.append((
-            f"  <g data-testid=\"language\" transform=\"translate({xoffset}, {yoffset})\">\n"
-        ))
-        x = 0
-        dx = 5
-        x += dx
-        radius = 0.5 * iconsize
-        langcolor = get_lang_color_dict(lang)
-        string.append((
-            f"    <circle cx=\"{x}\" cy=\"-5\" r=\"{radius}\" fill=\"{langcolor}\" />\n"
-        ))
-        dx = iconsize
-        x += dx
-        string.append((
-            f"    <text class=\"descr-text\" x=\"{x}\" y=\"0\">\n"
-            f"      {lang}\n"
-            f"    </text>\n"
-            f"  </g>\n"
-        ))
-        dx = (len(lang) + 4) * w_descr
-        x += dx
-        xoffset += x
-    # star
-    string.append((
-        f"  <g data-testid=\"star\" transform=\"translate({xoffset}, {yoffset})\">\n"
-    ))
-    string.append(draw_star(1.25 * iconsize))
-    dx = iconsize
-    xoffset += dx
-    string.append((
-        f"    <g data-testid=\"star-text\" transform=\"translate({dx}, 0)\">\n"
-        f"      <text class=\"descr-text\">\n"
-        f"        {nstars}\n"
-        f"      </text>\n"
-        f"    </g>\n"
-        f"  </g>\n"
-    ))
-    dx = (len(nstars) + 4) * w_descr
-    xoffset += dx
-    # date
-    string.append((
-        f"  <g data-testid=\"date\" transform=\"translate({xoffset}, {yoffset})\">\n"
-        f"    <text class=\"descr-text\">\n"
-        f"      Last update: {lastcommit}\n"
-        f"    </text>\n"
-        f"  </g>\n"
-    ))
-    string = "".join(string)
-    return string, yoffset + h_title
+            text += char
+            w += font_descr.w
+    else:
+        texts.append(text)
+    result = list()
+    for text in texts:
+        result.append(
+            {
+                "x": margin,
+                "y": domain["height"],
+                "text": text,
+            },
+        )
+        domain["height"] += 1.5 * font_descr.h
+    domain["height"] += 0.5 * font_descr.h
+    return result
 
-def create_card(info):
-    # width is fixed,
-    #   while height can vary
-    #   depending on the number of rows of
-    #   the repository description
-    width = 500
-    ## styles
-    # use Courier font, which is a monospace font
-    font = "Courier"
-    # width-height ratio, font-specific,
-    #   which is used to compute string width
-    rate = 0.607
-    # font size in terms of height, in pixels
-    h_title = 18 # px
-    h_descr = 13 # px
-    w_title = h_title * rate # px
-    w_descr = h_descr * rate # px
-    # icon size in pixel
-    iconsize = 16 # px
-    # x offset (left edge)
-    xoffset = 25
-    # first row: repository logo and title
-    yoffset = 35
-    row1st, yoffset = get_row1st(xoffset, yoffset, iconsize, info["name"])
-    # second row: repository description
-    row2nd, yoffset = get_row2nd(xoffset, yoffset, width, h_descr, w_descr, info["descr"])
-    # third row: primary language, number of stars, last update date
-    row3rd, height = get_row3rd(xoffset, yoffset, iconsize, w_descr, h_title, info)
-    # Now all parameters are ready
-    # get all svg elements using the computed parameters
-    header = get_header(width, height)
-    footer = get_footer()
-    styles = get_styles(font, h_title, h_descr)
-    edge_r = get_edge_r(width, height)
-    # open file and create svg
-    with open(f"card/{name}.svg", "w") as f:
-        f.write(header)
-        f.write(styles)
-        f.write(edge_r)
-        f.write(row1st)
-        f.write(row2nd)
-        f.write(row3rd)
-        f.write(footer)
+def get_topics(info: dict, domain: dict, margin: dict) -> list:
+    topics = info["topics"]
+    width = domain["width"] - 2 * margin - 3. * font_descr.w
+    x = margin + 0.5 * font_descr.w
+    y = domain["height"]
+    result = list()
+    for topic in topics:
+        dx = (len(topic) + 2) * font_descr.w
+        if x + dx > width:
+            x = margin + 0.5 * font_descr.w
+            y += 1.5 * font_descr.h
+            domain["height"] += 1.5 * font_descr.h
+        result.append({
+            "text": {
+                "x": x,
+                "y": y,
+                "text": topic,
+            },
+            "rect": {
+                "x": x - 0.5 * font_descr.w,
+                "y": y - 0.6 * font_descr.h,
+                "width": (len(topic) + 1) * font_descr.w,
+                "height": 1.2 * font_descr.h,
+                "round": 4,
+            },
+        })
+        x += dx
+    domain["height"] += 1.5 * font_title.h
+    return result
 
-if __name__ == "__main__":
+def get_3rd(info, domain: dict, margin: float) -> (dict, dict, dict):
+    distance = 2. * font_descr.w
+    def get_lang(domain: dict, x: float) -> (dict, float):
+        def get_lang_color_dict(key):
+            import os
+            import json
+            filename = f"{os.path.dirname(__file__)}/colortable.json"
+            with open(filename, "r") as fp:
+                table = json.load(fp)
+            if key in table.keys():
+                return table[key]
+            else:
+                # return black if not found
+                print("language color is not defined: ", key)
+                return "#000000"
+        r = 0.5 * font_descr.h
+        lang = dict()
+        lang["mark"] = {
+            "cx": x + r,
+            "cy": domain["height"],
+            "r": r,
+            "fill": get_lang_color_dict(info["lang"]),
+        }
+        x += 2.5 * r
+        lang["text"] = {
+            "x": x,
+            "y": domain["height"],
+            "text": info["lang"],
+        }
+        x += (len(info["lang"]) + 2) * font_descr.w
+        return (lang, x)
+    def get_star(domain: dict, x0: float) -> (dict, float):
+        ro = 0.5 * font_descr.h
+        y0 = domain["height"]
+        ri = 0.5 * ro
+        xs = list()
+        ys = list()
+        for cnt in range(10):
+            import math
+            t = 2. * math.pi * cnt / 10 - 0.5 * math.pi
+            r = ro if cnt % 2 == 0 else ri
+            xs.append(x0 + r * math.cos(t))
+            ys.append(y0 + r * math.sin(t))
+        path = ""
+        for cnt, (x, y) in enumerate(zip(xs, ys)):
+            prefix = "M" if cnt ==           0 else "L"
+            suffix = "Z" if cnt == len(xs) - 1 else " "
+            path += f"{prefix} {x} {y} {suffix}"
+        x0 += 1.5 * ro
+        star = {
+            "mark": path,
+            "text": {
+                "x": x0,
+                "y": domain["height"],
+                "text": info["nstars"],
+            },
+        }
+        x = x0 + (len(info["nstars"]) + 1.5) * font_descr.w
+        return (star, x)
+    def get_last_update(domain: dict, x: float) -> (dict, float):
+        last_update = {
+            "x": x,
+            "y": domain["height"],
+            "text": info["lastcommit"],
+        }
+        x += len(last_update["text"]) * font_descr.w
+        return (last_update, x)
+    x = margin
+    lang, x = get_lang(domain, x)
+    star, x = get_star(domain, x)
+    last_update, x = get_last_update(domain, x)
+    domain["height"] += 1.5 * font_title.h
+    return lang, star, last_update
+
+def get_border(domain: dict) -> dict:
+    stroke_width = 2
+    border = {
+        "x": 0.5 * stroke_width,
+        "y": 0.5 * stroke_width,
+        "width": domain["width"] - stroke_width,
+        "height": domain["height"] - stroke_width,
+        "round": 10,
+        "stroke_width": stroke_width,
+    }
+    return border
+
+def dump(filename: str, keywords: dict):
+    import os
+    import jinja2
+    tempname = f"{os.path.dirname(__file__)}/template.svg"
+    with open(tempname, "r") as f:
+        sample = f.readlines()
+    sample = "".join(sample)
+    template = jinja2.Template(source=sample)
+    result = template.render(**keywords)
+    with open(filename, "w") as f:
+        f.write(result)
+
+def create_card(path_out: str, info: dict):
+    domain = {
+        # fixed
+        "width": 500,
+        # subject to change
+        "height": 0,
+    }
+    margin = set_margin(domain)
+    title = get_title(info, domain, margin)
+    descrs = get_descrs(info, domain, margin)
+    topics = get_topics(info, domain, margin)
+    lang, star, last_update = get_3rd(info, domain, margin)
+    border = get_border(domain)
+    dump(
+        path_out,
+        {
+            "domain": domain,
+            "border": border,
+            "margin": margin,
+            "title": title,
+            "descrs": descrs,
+            "topics": topics,
+            "lang": lang,
+            "star": star,
+            "last_update": last_update,
+        },
+    )
+
+def main():
+    import sys
+    import json
     argv = sys.argv
     assert 2 == len(argv)
-    path = argv[1]
-    with open(path, "r") as f:
+    path_json = argv[1]
+    with open(path_json, "r") as f:
         categories = json.load(f)
     names = list()
     for category in categories:
@@ -338,5 +262,26 @@ if __name__ == "__main__":
     #   2. generate an svg card based on it
     for name in names:
         info = get_info(name)
-        create_card(info)
+        path_out = f"card/{info['name']}.svg"
+        create_card(path_out, info)
+
+def debug():
+    info = {
+        "name": "MyDebugRepository",
+        "descr": "Here is a description of this repository, which can be arbitrary long and thus if the sentence is properly new-line-d should be checked.",
+        "lang": "TypeScript",
+        "nstars": "10",
+        "lastcommit": "Jan 01 1970",
+        "topics": [
+            "cfd",
+            "fluid-dynamics",
+            "hpc",
+            "navier-stokes",
+        ],
+    }
+    path_out = "debug.svg"
+    create_card(path_out, info)
+
+main()
+# debug()
 
