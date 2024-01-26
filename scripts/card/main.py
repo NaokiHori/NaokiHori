87bce82effdd8f0dae13334c53d81135ff1f65cb
NaokiHori <36466440+NaokiHori@users.noreply.github.com>
2024-01-26 14:17:22 +0100

    [mod] card format

--- scripts/card/main.py
+++ scripts/card/main.py
@@ -19,11 +19,12 @@ def get_info(name: str):
         data = date.fromisoformat(data)
         data = data.strftime("%b %d %Y")
         return data
-    # extract repository name and its description
-    # ref: https://docs.github.com/en/rest/repos/repos?apiVersion=2022-11-28#get-a-repository
     import os
     import requests
+    info = dict()
     token = os.environ.get(key="GITHUB_TOKEN")
+    # extract repository name and its description
+    # ref: https://docs.github.com/en/rest/repos/repos?apiVersion=2022-11-28#get-a-repository
     r = requests.get(
             url=f"https://api.github.com/repos/NaokiHori/{name}",
             headers={
@@ -32,31 +33,80 @@ def get_info(name: str):
                 "X-GitHub-Api-Version": "2022-11-28",
             },
     )
+    if not r.ok:
+        print(r)
+        return info
     data = r.json()
     info = {
             "name": name,
             "descr": data["description"],
             "topics": data["topics"],
-            "lang": data["language"],
-            "nstars": str(data["stargazers_count"]),
+            "langs": [data["language"]],
             "lastcommit": get_lastcommit(data["updated_at"]),
     }
+    # try to fetch all languages
+    r = requests.get(
+            url=data["languages_url"],
+            headers={
+                "Accept": "application/vnd.github+json",
+                "Authorization": f"Bearer {token}",
+                "X-GitHub-Api-Version": "2022-11-28",
+            },
+    )
+    if not r.ok:
+        print(r)
+        return info
+    # here "key:value", take out key only, sorted by value
+    data = r.json()
+    info["langs"] = [k for k, v in sorted(data.items(), reverse=True, key=lambda x: x[1])]
     return info
+
+def make_boxed_items(items: list[str], domain: dict, margin: dict):
+    width = domain["width"] - 2 * margin
+    x0 = margin + 0.5 * font_descr.w
+    h_update = 2. * font_descr.h
+    x = x0
+    y = domain["height"]
+    result = list()
+    for item in items:
+        dx = (len(item) + 2) * font_descr.w
+        if x + dx > width:
+            x = x0
+            y += h_update
+            domain["height"] += h_update
+        rect_height = 1.4 * font_descr.h
+        result.append({
+            "text": {
+                "x": x,
+                "y": y,
+                "text": item,
+            },
+            "rect": {
+                "x": x - 0.5 * font_descr.w,
+                "y": y - 0.5 * rect_height,
+                "width": (len(item) + 1) * font_descr.w,
+                "height": rect_height,
+                "round": 4,
+            },
+        })
+        x += dx
+    domain["height"] += h_update
+    return result
 
 def set_margin(domain: dict) -> float:
     # left-right margin
-    result = 1.5 * font_title.w
-    return result
+    margin = 1.5 * font_title.w
+    return margin
 
 def get_title(info: dict, domain: dict, margin: float) -> dict:
     domain["height"] += 1.5 * font_title.h
-    result = {
+    title = {
         "text": info["name"],
         "x": margin,
         "y": domain["height"],
     }
     domain["height"] += 1.5 * font_title.h
-    return result
+    return title
 
 def get_descrs(info: dict, domain: dict, margin: float) -> list:
     width = domain["width"] - 2 * margin
@@ -68,16 +118,16 @@ def get_descrs(info: dict, domain: dict, margin: float) -> list:
     for char in chars:
         if w >= width:
             texts.append(text)
-            w = 0
+            w = font_descr.w
             text = char
         else:
             text += char
             w += font_descr.w
     else:
         texts.append(text)
-    result = list()
+    descrs = list()
     for text in texts:
-        result.append(
+        descrs.append(
             {
                 "x": margin,
                 "y": domain["height"],
@@ -86,111 +136,38 @@ def get_descrs(info: dict, domain: dict, margin: float) -> list:
         )
         domain["height"] += 1.5 * font_descr.h
     domain["height"] += 0.5 * font_descr.h
-    return result
+    return descrs
 
 def get_topics(info: dict, domain: dict, margin: dict) -> list:
-    topics = info["topics"]
-    width = domain["width"] - 2 * margin - 3. * font_descr.w
-    x = margin + 0.5 * font_descr.w
-    y = domain["height"]
-    result = list()
-    for topic in topics:
-        dx = (len(topic) + 2) * font_descr.w
-        if x + dx > width:
-            x = margin + 0.5 * font_descr.w
-            y += 1.5 * font_descr.h
-            domain["height"] += 1.5 * font_descr.h
-        result.append({
-            "text": {
-                "x": x,
-                "y": y,
-                "text": topic,
-            },
-            "rect": {
-                "x": x - 0.5 * font_descr.w,
-                "y": y - 0.6 * font_descr.h,
-                "width": (len(topic) + 1) * font_descr.w,
-                "height": 1.2 * font_descr.h,
-                "round": 4,
-            },
-        })
-        x += dx
-    domain["height"] += 1.5 * font_title.h
-    return result
+    topics = make_boxed_items(info["topics"], domain, margin)
+    return topics
 
-def get_3rd(info, domain: dict, margin: float) -> (dict, dict, dict):
-    distance = 2. * font_descr.w
-    def get_lang(domain: dict, x: float) -> (dict, float):
-        def get_lang_color_dict(key):
-            import os
-            import json
-            filename = f"{os.path.dirname(__file__)}/colortable.json"
-            with open(filename, "r") as fp:
-                table = json.load(fp)
-            if key in table.keys():
-                return table[key]
-            else:
-                # return black if not found
-                print("language color is not defined: ", key)
-                return "#000000"
-        r = 0.5 * font_descr.h
-        lang = dict()
-        lang["mark"] = {
-            "cx": x + r,
-            "cy": domain["height"],
-            "r": r,
-            "fill": get_lang_color_dict(info["lang"]),
-        }
-        x += 2.5 * r
-        lang["text"] = {
-            "x": x,
-            "y": domain["height"],
-            "text": info["lang"],
-        }
-        x += (len(info["lang"]) + 2) * font_descr.w
-        return (lang, x)
-    def get_star(domain: dict, x0: float) -> (dict, float):
-        ro = 0.5 * font_descr.h
-        y0 = domain["height"]
-        ri = 0.5 * ro
-        xs = list()
-        ys = list()
-        for cnt in range(10):
-            import math
-            t = 2. * math.pi * cnt / 10 - 0.5 * math.pi
-            r = ro if cnt % 2 == 0 else ri
-            xs.append(x0 + r * math.cos(t))
-            ys.append(y0 + r * math.sin(t))
-        path = ""
-        for cnt, (x, y) in enumerate(zip(xs, ys)):
-            prefix = "M" if cnt ==           0 else "L"
-            suffix = "Z" if cnt == len(xs) - 1 else " "
-            path += f"{prefix} {x} {y} {suffix}"
-        x0 += 1.5 * ro
-        star = {
-            "mark": path,
-            "text": {
-                "x": x0,
-                "y": domain["height"],
-                "text": info["nstars"],
-            },
-        }
-        x = x0 + (len(info["nstars"]) + 1.5) * font_descr.w
-        return (star, x)
-    def get_last_update(domain: dict, x: float) -> (dict, float):
-        last_update = {
-            "x": x,
-            "y": domain["height"],
-            "text": info["lastcommit"],
-        }
-        x += len(last_update["text"]) * font_descr.w
-        return (last_update, x)
-    x = margin
-    lang, x = get_lang(domain, x)
-    star, x = get_star(domain, x)
-    last_update, x = get_last_update(domain, x)
+def get_langs(info: dict, domain: dict, margin: float) -> dict:
+    def get_lang_color(key: str):
+        import os
+        import json
+        filename = f"{os.path.dirname(__file__)}/colortable.json"
+        with open(filename, "r") as fp:
+            table = json.load(fp)
+        if key in table.keys():
+            return table[key]
+        else:
+            # return black if not found
+            print("language color is not defined: ", key)
+            return "#000000"
+    langs = make_boxed_items(info["langs"], domain, margin)
+    for lang in langs:
+        lang["rect"]["stroke"] = get_lang_color(lang["text"]["text"])
+    return langs
+
+def get_updated(info: dict, domain: dict, margin: float) -> dict:
+    updated = {
+        "x": margin,
+        "y": domain["height"],
+        "text": info["lastcommit"],
+    }
     domain["height"] += 1.5 * font_title.h
-    return lang, star, last_update
+    return updated
 
 def get_border(domain: dict) -> dict:
     stroke_width = 2
@@ -219,7 +196,7 @@ def dump(filename: str, keywords: dict):
 def create_card(path_out: str, info: dict):
     domain = {
         # fixed
-        "width": 500,
+        "width": 700,
         # subject to change
         "height": 0,
     }
@@ -227,7 +204,8 @@ def create_card(path_out: str, info: dict):
     title = get_title(info, domain, margin)
     descrs = get_descrs(info, domain, margin)
     topics = get_topics(info, domain, margin)
-    lang, star, last_update = get_3rd(info, domain, margin)
+    langs = get_langs(info, domain, margin)
+    updated = get_updated(info, domain, margin)
     border = get_border(domain)
     dump(
         path_out,
@@ -238,9 +216,8 @@ def create_card(path_out: str, info: dict):
             "title": title,
             "descrs": descrs,
             "topics": topics,
-            "lang": lang,
-            "star": star,
-            "last_update": last_update,
+            "langs": langs,
+            "updated": updated,
         },
     )
 
@@ -262,21 +239,49 @@ def main():
     #   2. generate an svg card based on it
     for name in names:
         info = get_info(name)
-        path_out = f"card/{info['name']}.svg"
+        if not info:
+            continue
+        path_out = "card/" + info["name"] + ".svg"
         create_card(path_out, info)
 
 def debug():
     info = {
         "name": "MyDebugRepository",
-        "descr": "Here is a description of this repository, which can be arbitrary long and thus if the sentence is properly new-line-d should be checked.",
-        "lang": "TypeScript",
-        "nstars": "10",
+        "descr": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        "langs": [
+            "C",
+            "Python",
+            "Rust",
+            "Makefile",
+            "Shell",
+            "HTML",
+            "CSS",
+            "WebAssembly",
+        ],
         "lastcommit": "Jan 01 1970",
         "topics": [
-            "cfd",
-            "fluid-dynamics",
-            "hpc",
-            "navier-stokes",
+            "Lorem",
+            "ipsum",
+            "dolor",
+            "sit",
+            "amet",
+            "unbelievably-long-ever-lasting-topic-element",
+            "consectetur",
+            "adipiscing",
+            "elit",
+            "sed",
+            "do",
+            "eiusmod",
+            "tempor",
+            "incididunt",
+            "ut",
+            "labore",
+            "et",
+            "unbelievably-long-ever-lasting-topic-element",
+            "dolore",
+            "magna",
+            "unbelievably-long-ever-lasting-topic-element",
+            "aliqua",
         ],
     }
     path_out = "debug.svg"
